11f7b374f18bd1c9856cd2e739e08332dc464c73
Lenny <lenni.trejos@gopenux.com>
2023-08-28 15:27:52 -0500

    Se agrega la característica de vinculación de vistas (viewBinding)

--- app/src/main/java/com/gopenux/myanimelist/MainActivity.java
+++ app/src/main/java/com/gopenux/myanimelist/MainActivity.java
@@ -3,12 +3,18 @@ package com.gopenux.myanimelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.gopenux.myanimelist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
